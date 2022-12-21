a6a7fafa90f155be341ce8e3984544128eb29e37
Ivan Petrov <ivannikopol86@gmail.com>
2022-12-21 17:00:05 +0300

    Ne

--- src/HelloNew/HelloNew.java
+++ src/HelloNew/HelloNew.java
@@ -4,7 +4,8 @@ import java.sql.SQLOutput;
 
 public class HelloNew {
     public static void main(String[] args) {
-        System.out.println("Hello New");
-        System.out.println("one more time");
+        boolean x = true;
+        System.out.println(x);
+
     }
 }
