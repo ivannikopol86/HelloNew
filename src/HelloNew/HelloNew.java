79b2ae5f818d7573efba4760502b6d63e36e9f0b
Ivan Petrov <ivannikopol86@gmail.com>
2022-12-21 16:27:11 +0300

    New2

--- src/HelloNew/HelloNew.java
+++ src/HelloNew/HelloNew.java
@@ -1,7 +1,10 @@
 package HelloNew;
 
+import java.sql.SQLOutput;
+
 public class HelloNew {
     public static void main(String[] args) {
         System.out.println("Hello New");
+        System.out.println("one more time");
     }
 }
